38ec1e5749c5fc93a8a201246e3eabfb79b5dc21
printfn <printfn@users.noreply.github.com>
2022-02-24 10:57:16 +1300

    Initial commit

---
+++ Formula/checkmake.rb
@@ -0,0 +1,15 @@
+class Checkmake < Formula
+  desc "Linter/analyzer for Makefiles"
+  homepage "https://github.com/mrtazz/checkmake"
+  url "https://github.com/mrtazz/checkmake/archive/refs/tags/0.2.1.tar.gz"
+  sha256 "6e0d5237bc1de2a42ba1cf1a5c1da7d783bd9da06755e0c7faba6c3ba77ab1ee"
+  license "MIT"
+  head "https://github.com/mrtazz/checkmake.git", branch: "main"
+
+  depends_on "pandoc"
+  depends_on "go" => :build
+
+  def install
+    system "make"
+  end
+end
