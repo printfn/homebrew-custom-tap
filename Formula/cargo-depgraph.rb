40442b66e2136e41e5c0900bd812227839658b08
printfn <printfn@users.noreply.github.com>
2022-03-13 12:15:11 +1300

    Add test

--- Formula/cargo-depgraph.rb
+++ Formula/cargo-depgraph.rb
@@ -5,18 +5,33 @@ class CargoDepgraph < Formula
   sha256 "7f56739ca920d3be998bfd2365fd0f5d468b9297352269ea043169ae87005349"
   license "GPL-3.0-or-later"
 
-  depends_on "rust" => :build
-
   livecheck do
     url :stable
     regex(/^v?(\d+(?:\.\d+)+)$/i)
   end
+
+  depends_on "rust" => :build
 
   def install
     system "cargo", "install", *std_cargo_args
   end
 
   test do
-    system "false"
+    crate = testpath/"demo-crate"
+    mkdir crate do
+      (crate/"src/main.rs").write "// Dummy file"
+      (crate/"Cargo.toml").write <<~EOS
+        [package]
+        name = "demo-crate"
+        version = "0.1.0"
+      EOS
+      expected = <<~EOS
+        digraph {
+            0 [ label = "demo-crate" shape = box]
+        }
+
+      EOS
+      assert_equal expected, shell_output("PATH=#{Formula["rust"].bin}:$PATH cargo depgraph")
+    end
   end
 end
