296d632f31e8b29d326459af9fdd6f554a37f276
printfn <printfn@users.noreply.github.com>
2022-04-04 12:12:54 +1200

    hextoggle v1.0.5

--- Formula/hextoggle.rb
+++ Formula/hextoggle.rb
@@ -1,8 +1,8 @@
 class Hextoggle < Formula
   desc "Easily view and edit hex files"
   homepage "https://github.com/printfn/hextoggle"
-  url "https://github.com/printfn/hextoggle/archive/refs/tags/v1.0.4.tar.gz", branch: "main"
-  sha256 "c6d8155a4e20d29816df3f244eed0e09ece2a58f694410bb0ad1107427befbad"
+  url "https://github.com/printfn/hextoggle/archive/refs/tags/v1.0.5.tar.gz", branch: "main"
+  sha256 "b06fe39ff8949e6b59491327c319a2a26a15a0b40ad4f80ca4cfae1b6329435e"
   license "GPL-3.0-or-later"
   head "https://github.com/printfn/hextoggle.git", branch: "main"
 
