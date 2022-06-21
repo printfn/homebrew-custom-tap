f0ee41046fd6541d9f58e22a9e81d2b7f7f2cde6
printfn <printfn@users.noreply.github.com>
2022-06-21 07:14:41 +0200

    Add steghide (broken)

---
+++ Formula/steghide.rb
@@ -0,0 +1,22 @@
+class Steghide < Formula
+  desc "Steganography program that is able to hide data in various kinds of image- and audio-files"
+  homepage "http://steghide.sourceforge.net/"
+  url "http://prdownloads.sourceforge.net/steghide/steghide-0.5.1.tar.gz"
+  sha256 "78069b7cfe9d1f5348ae43f918f06f91d783c2b3ff25af021e6a312cf541b47b"
+  license "GPL-2.0-or-later"
+
+  depends_on "gettext"
+  depends_on "mcrypt"
+  depends_on "mhash"
+
+  def install
+    system "./configure", *std_configure_args
+    system "make"
+    system "make", "check"
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
