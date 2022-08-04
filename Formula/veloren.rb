8d3f1d8e5399b6e9f8ec10f264b853e1937f8306
printfn <printfn@users.noreply.github.com>
2022-08-05 10:12:40 +1200

    Fix veloren

--- Formula/veloren.rb
+++ Formula/veloren.rb
@@ -1,22 +1,30 @@
 class Veloren < Formula
   desc "Multiplayer voxel RPG"
   homepage "https://veloren.net"
-  url "https://gitlab.com/veloren/veloren/-/archive/v0.13.0/veloren-v0.13.0.tar.gz"
-  sha256 "6864e9ac190b07eae7d5ea966472d120037153af086f64cc8a7966a59a5f2cfe"
+  url "https://gitlab.com/veloren/veloren.git",
+    tag:      "v0.13.0",
+    revision: "34ae5b9df73f367fa34350337ebba71711f48b86"
   license "GPL-3.0-or-later"
 
   depends_on "cmake" => :build
+  depends_on "git-lfs" => :build
   depends_on "ninja" => :build
   depends_on "rustup-init" => :build
 
   def install
+    system "git", "lfs", "install", "--local"
+    system "git", "lfs", "pull"
+
+    # make sure the assets have been downloaded correctly with git-lfs
+    unless `file assets/voxygen/background/bg_main.jpg`.match?(/JPEG image/i)
+      odie "assets have not been downloaded correctly"
+    end
+
     # this will install the necessary cargo/rustup toolchain bits in HOMEBREW_CACHE
     system Formula["rustup-init"].bin/"rustup-init", "-qy", "--no-modify-path"
     ENV.prepend_path "PATH", HOMEBREW_CACHE/"cargo_cache/bin"
 
     ENV["VELOREN_USERDATA_STRATEGY"] = "system"
-    ENV["NIX_GIT_HASH"] = "00000000/#{time.strftime("%Y-%m-%d-%H:%M")}"
-    ENV["NIX_GIT_TAG"] = "v#{version}"
 
     system "cargo", "install", *std_cargo_args(root: libexec, path: "voxygen")
     system "cargo", "install", *std_cargo_args(root: libexec, path: "server-cli")
@@ -30,7 +38,7 @@ class Veloren < Formula
   end
 
   test do
-    system "false"
-    # assert_match "Airshipper #{version}", shell_output("#{bin}/airshipper --version")
+    output = shell_output("echo \"shutdown graceful 0\" | #{bin}/veloren-server-cli")
+    assert_match "Server is ready to accept connections", output
   end
 end
