41b2e0360b66600b97b38c1926df8bf8420d2baf
printfn <printfn@users.noreply.github.com>
2022-08-06 01:08:34 +1200

    Fix airshipper

--- Formula/airshipper.rb
+++ Formula/airshipper.rb
@@ -1,5 +1,5 @@
 class Airshipper < Formula
-  desc "Installer for Veloren"
+  desc "Installer for Veloren, a multiplayer voxel RPG"
   homepage "https://veloren.net"
   url "https://gitlab.com/veloren/airshipper/-/archive/v0.7.0/airshipper-v0.7.0.tar.gz"
   sha256 "dd68535fe6a7a4f01186b590c33e7b12bc79c8aa8a96529e22f9321ea2a06325"
@@ -9,9 +9,10 @@ class Airshipper < Formula
 
   def install
     system "cargo", "install", *std_cargo_args(path: "client")
+    system "cargo", "install", *std_cargo_args(path: "server")
   end
 
   test do
-    assert_match "Airshipper #{version}", shell_output("#{bin}/airshipper --version")
+    assert_match "Done", shell_output("#{bin}/airshipper update")
   end
 end
