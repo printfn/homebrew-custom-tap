ae2069bf053e799b1dee93bf20c7761b063f41b0
printfn <printfn@users.noreply.github.com>
2022-08-09 22:47:59 +1200

    Update airshipper description

--- Formula/airshipper.rb
+++ Formula/airshipper.rb
@@ -1,5 +1,5 @@
 class Airshipper < Formula
-  desc "Installer for Veloren, a multiplayer voxel RPG"
+  desc "Launcher for Veloren, a multiplayer voxel RPG"
   homepage "https://veloren.net"
   url "https://gitlab.com/veloren/airshipper/-/archive/v0.7.0/airshipper-v0.7.0.tar.gz"
   sha256 "dd68535fe6a7a4f01186b590c33e7b12bc79c8aa8a96529e22f9321ea2a06325"
