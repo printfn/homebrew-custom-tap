20b4d4a030f337e506c7f6d1ad4ff6e022829c2b
printfn <printfn@users.noreply.github.com>
2022-08-11 16:31:44 +1200

    veloren: add bottles

--- Formula/airshipper.rb
+++ Formula/airshipper.rb
@@ -5,6 +5,12 @@ class Airshipper < Formula
   sha256 "dd68535fe6a7a4f01186b590c33e7b12bc79c8aa8a96529e22f9321ea2a06325"
   license "GPL-3.0-or-later"
 
+  bottle do
+    root_url "https://github.com/printfn/homebrew-custom-tap/releases/download/veloren-0.7.0"
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "ee61536b9f373420adf1b27a4dcf551b807fc75aef18c50256d4547cbe1de3fe"
+    sha256 cellar: :any_skip_relocation, monterey:       "d4b4c121fc927e19c2043ad7d7aea77021240333bd78ce821070f3d015b88fec"
+  end
+
   depends_on "rust" => :build
 
   def install
