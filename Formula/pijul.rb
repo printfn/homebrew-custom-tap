fd31cdfeab925c4ee5f2ee4dff67f4b7d9f9d66b
printfn <printfn@users.noreply.github.com>
2022-08-16 19:17:25 +1200

    Fix audit warnings

--- Formula/pijul.rb
+++ Formula/pijul.rb
@@ -1,5 +1,5 @@
 class Pijul < Formula
-  desc "The sound distributed version control system"
+  desc "Sound distributed version control system"
   homepage "https://nest.pijul.com/pijul/pijul"
   url "https://static.crates.io/crates/pijul/pijul-1.0.0-beta.2.crate"
   sha256 "9e0870af813cdab0a8f8f3687f545714fc0f6aeee47a2447bea3bf3ce6706dc5"
@@ -7,12 +7,11 @@ class Pijul < Formula
 
   livecheck do
     url "https://crates.io/api/v1/crates/pijul/versions"
-    #regex(/"num":\s*"(\d+(?:\.\d+)+)"/i)
     regex(/"num":\s*"(\d+(?:\.\d+|-beta)+)"/i)
   end
 
-  depends_on "nettle"
   depends_on "rust" => :build
+  depends_on "nettle"
   depends_on "xxhash"
 
   def install
--- Formula/steghide.rb
+++ Formula/steghide.rb
@@ -1,7 +1,7 @@
 class Steghide < Formula
-  desc "Steganography program that is able to hide data in various kinds of image- and audio-files"
-  homepage "http://steghide.sourceforge.net/"
-  url "http://prdownloads.sourceforge.net/steghide/steghide-0.5.1.tar.gz"
+  desc "Hide data in various kinds of image and audio files"
+  homepage "https://steghide.sourceforge.io/"
+  url "https://downloads.sourceforge.net/project/steghide/steghide/0.5.1/steghide-0.5.1.tar.gz"
   sha256 "78069b7cfe9d1f5348ae43f918f06f91d783c2b3ff25af021e6a312cf541b47b"
   license "GPL-2.0-or-later"
 
